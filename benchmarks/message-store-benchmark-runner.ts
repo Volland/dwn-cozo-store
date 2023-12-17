f2a31a8f72082a20143c81ec8fa9bc5d5213eedd
Volodymyr Pavlyshyn <pavlyshyn@gmail.com>
2023-12-17 13:59:45 +0100

    fix imports

--- benchmarks/message-store-benchmark-runner.ts
+++ benchmarks/message-store-benchmark-runner.ts
@@ -1,11 +1,8 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
 import { CozoClosableAdapter } from '../src/adapters/cozo-closable-adapter.js';
 import { MessageStoreCozo } from '../src/message-store-cozo.js';
 import { runMessageStoreBenchmark } from './message-store-benchmark.js';
 
-const { MessageStoreLevel } = require('@tbd54566975/dwn-sdk-js/dist/esm/src/store/message-store-level.js');
+import { MessageStoreLevel } from '@tbd54566975/dwn-sdk-js/dist/esm/src/store/message-store-level.js';
 
 const rocksdb = new CozoClosableAdapter('rocksdb', './rocksdb');
 const results = {};
